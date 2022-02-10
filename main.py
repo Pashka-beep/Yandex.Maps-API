d127acf513cac840f52534ed2c857a848890e3bf
cheloveg <cheloveg@localhost>
2022-02-10 19:58:49 +0300

    PAGEUP and PAGEDOWN support(maybe)

--- main.py
+++ main.py
@@ -4,35 +4,48 @@ import sys
 import os
 
 
-api_server = "http://static-maps.yandex.ru/1.x/"
+def update(lon, lat, z):
+    api_server = "http://static-maps.yandex.ru/1.x/"
 
-lon = "37.530887"
-lat = "55.703118"
+    params = {
+        "ll": ",".join([str(lon), str(lat)]),
+        "z": 8,
+        "l": "map"
+    }
+    response = requests.get(api_server, params=params)
 
-params = {
-    "ll": ",".join([lon, lat]),
-    "z": 8,
-    "l": "map"
-}
-response = requests.get(api_server, params=params)
+    if not response:
+        pass
 
-if not response:
-    pass
+    with open('tmp.png', 'wb') as image_file:
+        image_file.write(response.content)
+
+    sc = pygame.display.set_mode((600, 450))
+    maps = pygame.image.load('tmp.png')
+    maps_rect = maps.get_rect()
+    sc.blit(maps, maps_rect)
+    pygame.display.update()
 
 
-with open('tmp.png', 'wb') as image_file:
-    image_file.write(response.content)
+lon = 37.530887
+lat = 55.703118
+z = 17
+changed = False
+pygame.init()
+update(lon, lat, z)
 
-sc = pygame.display.set_mode((600, 450))
-maps = pygame.image.load('tmp.png')
-maps_rect = maps.get_rect()
-sc.blit(maps, maps_rect)
-pygame.display.update()
 while 1:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             os.remove('tmp.png')
             sys.exit()
-        if i.type == pygame.KEYDOWN and i.key == pygame.K_PAGEUP:
-            params['z'] += 1
+        elif i.type == pygame.KEYDOWN and i.key == pygame.K_PAGEUP and z < 17:
+            z += 1
+            changed = True
+        elif i.type == pygame.KEYDOWN and i.key == pygame.K_PAGEDOWN and z > 0:
+            z -= 1
+            changed = True
+
+        if changed:
+            update(lon, lat, z)
 
