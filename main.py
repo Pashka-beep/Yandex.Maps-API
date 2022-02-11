bd1fe093e3f361ec853bd1779263fd60b458390c
cheloveg <cheloveg@localhost>
2022-02-11 08:52:02 +0300

    UP DOWN LEFT RIGHT works

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import pygame
 import sys
 import os
+import math
 
 
 def update(lon, lat, z):
@@ -12,6 +13,7 @@ def update(lon, lat, z):
         "z": z,
         "l": "map"
     }
+
     response = requests.get(api_server, params=params)
 
     if not response:
@@ -26,6 +28,12 @@ def update(lon, lat, z):
     sc.blit(maps, maps_rect)
     pygame.display.update()
 
+
+# Подобранные константы для поведения карты.
+LAT_STEP = 0.008 * 5  # Шаги при движении карты по широте и долготе
+LON_STEP = 0.002 * 5
+coord_to_geo_x = 0.0000428  # Пропорции пиксельных и географических координат.
+coord_to_geo_y = 0.0000428
 
 lon = 37.530887
 lat = 55.703118
@@ -44,6 +52,18 @@ while 1:
             changed = True
         elif i.type == pygame.KEYDOWN and i.key == pygame.K_PAGEDOWN and z > 0:
             z -= 1
+            changed = True
+        elif i.type == pygame.KEYDOWN and i.key == pygame.K_LEFT:
+            lon -= LON_STEP * math.pow(2, 15 - z)
+            changed = True
+        elif i.type == pygame.KEYDOWN and i.key == pygame.K_RIGHT:
+            lon += LON_STEP * math.pow(2, 15 - z)
+            changed = True
+        elif i.type == pygame.KEYDOWN and i.key == pygame.K_UP:
+            lat -= LAT_STEP * math.pow(2, 15 - z)
+            changed = True
+        elif i.type == pygame.KEYDOWN and i.key == pygame.K_DOWN:
+            lat += LAT_STEP * math.pow(2, 15 - z)
             changed = True
 
         if changed:
